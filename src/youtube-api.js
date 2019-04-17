3766aec2b148ecc17584eaec6a4e5b16cb50370b
Stephen Crowe <crowe.stephen.m@gmail.com>
2019-04-17 17:26:56 -0400

    workshop

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR API KEY'; // add your own key here
+const API_KEY = 'AIzaSyD0DHbicej8UckwaXQbrlAsF-sfGcBkbPg'; // add your own key here
 
 const youtubeSearch = (term) => {
   const params = {
